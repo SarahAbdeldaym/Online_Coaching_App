bbdf990b77f7cd6f37a32c90d1b755eda2655502
hossamadel23895 <hossamadel23895@gmail.com>
2022-07-12 18:53:00 +0200

    Adding Admin seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Illuminate\Support\Carbon;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('admins')->insert([
+            'name_en' => 'admin',
+            'name_ar' => 'مدير',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make(12345678),
+            'remember_token' => Str::random(10),
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ],[
+            'name_en' => 'Super Admin',
+            'name_ar' => 'عميد',
+            'email' => 'SuperAdmin@SuperAdmin.com',
+            'password' => Hash::make(123456),
+            'remember_token' => Str::random(10),
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+    }
+}
