c443cb606f91c1193d4138c0817dcbb7211709ce
SarahAbdeldaym <sarahabdeldaym@gmail.com>
2022-07-13 22:50:00 +0200

    Adding data to database

---
+++ database/data/countries.php
@@ -0,0 +1,7 @@
+<?php
+return array(
+    array('code' => 'US', 'name_ar' => '', 'name_en' => 'United States', 'created_at' => now(), 'updated_at' => now()),
+    array('code' => 'CA', 'name_ar' => '', 'name_en' => 'Canada', 'created_at' => now(), 'updated_at' => now()),
+    array('code' => 'GR', 'name_ar' => '', 'name_en' => 'Germany', 'created_at' => now(), 'updated_at' => now()),
+    array('code' => 'RU', 'name_ar' => '', 'name_en' => 'Russia', 'created_at' => now(), 'updated_at' => now()),
+);
